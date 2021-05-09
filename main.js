a93e28787bb77262321385acfe45ce9daecbf5f7
m-fidow <muna.a.fidow@gmail.com>
2021-05-09 14:58:47 +0100

    first part of pseudocode and fetchrecipe function

--- main.js
+++ main.js
@@ -9,6 +9,22 @@ entered as a search query.
 with the id #recipe-link to be the recipe label from the data you receive.
 4. Then set the href of #recipe-link to be the recipe's url,
  also from the received data.
+
+PSEUDOCODE
+IN FUNCTION FETCH_RECIPE
+  - AWAIT FETCH RESPONSE WITH URL
+    - USE STRING INTERPOLATION TO INJECT FOOD_TO_SEARCH
+  - SET AS RESPONSE
+  - AWAIT RESPONSE 
+  - APPLY JSON() METHOD TO RETRIEVE JS OBJECT
+  - SET AS DATA
+  - RETURN FIRST HIT OF DATA ARRAY
+  - SET ASS FIRST_RECIPE
+  - RETURN FIRST_RECIPE
+
+
+
+
 */
 const YOUR_APP_ID = "ac4c321f";
 const YOUR_APP_KEY = "90ba872d5ada15d1e7ae658122a03d3b";
@@ -28,10 +44,11 @@ function handleFoodChange() {
 async function fetchRecipe(food) {
   //--- write your code below ---
   let response = await fetch(
-    "https://api.edamam.com/search?q=kale&app_id=ac4c321f&app_key=90ba872d5ada15d1e7ae658122a03d3b"
+    `https://api.edamam.com/search?q=${foodToSearch}&app_id=ac4c321f&app_key=90ba872d5ada15d1e7ae658122a03d3b`
   );
   let data = await response.json();
+  let firstRecipe = data.hits[0];
 
-  console.log(data.hits[3]);
+  console.log(firstRecipe);
   //--- write your code above ---
 }
