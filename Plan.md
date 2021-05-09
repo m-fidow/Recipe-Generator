b3d85d818aac6f9005b27e11214d3d35e21a5b48
m-fidow <muna.a.fidow@gmail.com>
2021-05-09 13:55:19 +0100

    started planning file. Started planning on JS

---
+++ Plan.md
@@ -0,0 +1,16 @@
+Phase 1
+
+1. Use the fetchRecipe function to make a fetch request
+   to the Edamam API with
+   the food that the function takes in
+   entered as a search query.
+2. Use the first recipe from the hits array in the data that you receive.
+3. Use .innerHTML to set the text contained in the a tag
+   with the id #recipe-link to be the recipe label from the data you receive.
+4. Then set the href of #recipe-link to be the recipe's url,
+   also from the received data.
+   Phase 2
+5. Can you add images, ingredients or other information to your page
+6. Can you have more than one recipe appear on the page
+   with the information so people can pick from a selection
+7. Add a CSS file and style your page appropriately.
--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+/*
+Phase 1
+1. Use the fetchRecipe function to make a fetch request
+to the Edamam API with 
+the food that the function takes in 
+entered as a search query. 
+2. Use the first recipe from the hits array in the data that you receive.
+3. Use .innerHTML to set the text contained in the a tag 
+with the id #recipe-link to be the recipe label from the data you receive.
+4. Then set the href of #recipe-link to be the recipe's url,
+ also from the received data.
+*/
+const YOUR_APP_ID = "ac4c321f";
+const YOUR_APP_KEY = "90ba872d5ada15d1e7ae658122a03d3b";
+
+const requestUrl = `https://api.edamam.com/search?q=kale&app_id=ac4c321f&app_key=90ba872d5ada15d1e7ae658122a03d3b`;
+
 let foodToSearch = null;
 
 function handleRecipeClick() {
@@ -10,5 +27,11 @@ function handleFoodChange() {
 
 async function fetchRecipe(food) {
   //--- write your code below ---
+  let response = await fetch(
+    "https://api.edamam.com/search?q=kale&app_id=ac4c321f&app_key=90ba872d5ada15d1e7ae658122a03d3b"
+  );
+  let data = await response.json();
+
+  console.log(data.hits[3]);
   //--- write your code above ---
 }
